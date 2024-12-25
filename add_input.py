5f7b70159a78d321767283f994071f14931157e4
Nikolay Galkin <nicolasgalkin84@gmail.com>
2024-12-25 15:06:51 +0300

    Create add_input.py

---
+++ add_input.py
@@ -0,0 +1,12 @@
+username=input()
+#"nicolasgalkin84@gmail.com"
+title=input()
+#"Заголовок замметки"
+contest=input()
+#"Текст заметки"
+status=input()
+#"Готово"
+created_date=input()
+#("25-12-2024"))
+issue_date=input()
+#("25-12-2024")
