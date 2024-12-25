332478a9d3ad24baaa571d0495d968dccd5fd7db
Nikolay Galkin <nicolasgalkin84@gmail.com>
2024-12-25 14:53:58 +0300

    Create date_changer.py

---
+++ date_changer.py
@@ -0,0 +1,8 @@
+username="nicolasgalkin84@gmail.com"
+title="Заголовок замметки"
+contest="Текст заметки"
+status="Готово"
+created_date=("25-12-2024")
+issue_date=("25-12-2024")
+print (username, title, contest, status, created_date[0:5], issue_date[0:5])
+print (username, title, contest, status, created_date[:-5], issue_date[:-5])
