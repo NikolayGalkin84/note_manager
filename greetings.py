d2880d3ed8b3156f76cc97e19ca16b886c13a917
Nikolay Galkin <nicolasgalkin84@gmail.com>
2024-12-25 14:35:07 +0300

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -1,6 +1,7 @@
-username - nicolasgalkin84@gmail.com
-title
-contest
-ststus
-created_date
-issue_date
+username="nicolasgalkin84@gmail.com"
+title="Заголовок замметки"
+contest="Текст заметки"
+status="Готово"
+created_date=("25-12-24")
+issue_date=("25-12-24")
+print (username, title, contest, status, created_date, issue_date)
