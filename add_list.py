bba12088eb82c6d2857f2805741e88d8d9e33298
Nikolay Galkin <nicolasgalkin84@gmail.com>
2024-12-27 10:13:12 +0300

    Update add_list.py

--- add_list.py
+++ add_list.py
@@ -1,1 +1,11 @@
-
+username="nicolasgalkin84@gmail.com"
+titles=[]
+titles_length=3
+for idx in range(titles_length):
+    item = input('Enter title: ')
+    titles.append(item)
+contest="Текст заметки"
+status="Готово"
+created_date=("25-12-2024")
+issue_date=("25-12-2024")
+print (username,titles, contest, status, created_date, issue_date)
